96201566387f8063058204e72f2fd75df07b6941
Chkhikvadze <giga.chkhikvadze@gmail.com>
2023-12-26 22:19:41 +0400

    refactor validot classes

--- neurons/validators/twitter_validator.py
+++ neurons/validators/twitter_validator.py
@@ -24,7 +24,7 @@ from utils.tasks import Task, TwitterTask
 from template.utils import get_random_tweet_prompts
 from template.services.twilio import TwitterAPIClient
 
-class TwitterScraperStreamingValidator(BaseValidator):
+class TwitterScraperValidator(BaseValidator):
     def __init__(self, dendrite, config, subtensor, wallet):
         super().__init__(dendrite, config, subtensor, wallet, timeout=60)
         self.streaming = True
@@ -186,7 +186,6 @@ class TwitterScraperStreamingValidator(BaseValidator):
                 event[penalty_fn_i.name + "_applied"] = applied_penalty_i.tolist()
             bt.logging.trace(str(penalty_fn_i.name), applied_penalty_i.tolist())
 
-
         # # Find the best completion given the rewards vector.
         # completions: List[str] = [comp.completion for comp in responses]
         # completion_status_message: List[str] = [
@@ -270,9 +269,10 @@ class TwitterScraperStreamingValidator(BaseValidator):
                 except json.JSONDecodeError:
                     bt.logging.trace(f"Failed to decode JSON chunk: {resp}")
 
-    async def organic(self, metagraph, query):
+    async def organic(self, query, available_uids, metagraph):
         prompt = query['content']
-        uid = 1
+        uid_list = list(available_uids.keys())
+        uid = torch.tensor([random.choice(uid_list)]) if uid_list else torch.tensor([])
         # messages.append()
         syn = TwitterScraperStreaming(messages=prompt, model=self.model, seed=self.seed)
         bt.logging.info(f"Sending {syn.model} {self.query_type} request to uid: {uid}, timeout {self.timeout}: {syn.messages}")
@@ -282,122 +282,11 @@ class TwitterScraperStreamingValidator(BaseValidator):
         async for response in self.return_tokens(uid, responses):
             yield response
 
-    # async def run_task_and_score(self, task: Task, available_uids, metagraph):
-    #     task_name = task.task_name
-    #     prompt = task.compose_prompt()
-
-    #     bt.logging.debug("run_task", task_name)
-
-    #     # Record event start time.
-    #     event = {"name": task_name, "task_type": task.task_type}
-    #     start_time = time.time()
-    #     # Get the list of uids to query for this step.
-
-    #     av_uids = [uid for uid in available_uids]
-    #     uids = torch.tensor(random.sample(av_uids, len(av_uids)))
-    #     # uids = get_random_uids(self, k=k, exclude=exclude).to(self.device)
-    #     axons = [metagraph.axons[uid] for uid in uids] #temp todo
-    #     synapse = TwitterScraperStreaming(messages=prompt, model=self.model, seed=self.seed)
-
-    #     # Make calls to the network with the prompt.
-    #     async_responses: List[bt.Synapse] = await self.dendrite(
-    #         axons=axons,
-    #         synapse=synapse,
-    #         timeout=self.timeout,
-    #         streaming=self.streaming,
-    #         deserialize=False,
-    #     )
-
-    #     responses = await self.process_async_responses(async_responses)
-
-    #     # Compute the rewards for the responses given the prompt.
-    #     rewards: torch.FloatTensor = torch.zeros(len(responses), dtype=torch.float32).to(
-    #         self.device
-    #     )
-    #     for weight_i, reward_fn_i in zip(self.reward_weights, self.reward_functions):
-    #         reward_i_normalized, reward_event = reward_fn_i.apply(
-    #             task.base_text, responses, task_name
-    #         )
-    #         rewards += weight_i * reward_i_normalized.to(self.device)
-    #         if not self.config.neuron.disable_log_rewards:
-    #             event = {**event, **reward_event}
-    #         bt.logging.trace(str(reward_fn_i.name), reward_i_normalized.tolist())
-
-    #     for penalty_fn_i in self.penalty_functions:
-    #         (
-    #             raw_penalty_i,
-    #             adjusted_penalty_i,
-    #             applied_penalty_i,
-    #         ) = penalty_fn_i.apply_penalties(responses, task)
-    #         rewards *= applied_penalty_i.to(self.device)
-    #         if not self.config.neuron.disable_log_rewards:
-    #             event[penalty_fn_i.name + "_raw"] = raw_penalty_i.tolist()
-    #             event[penalty_fn_i.name + "_adjusted"] = adjusted_penalty_i.tolist()
-    #             event[penalty_fn_i.name + "_applied"] = applied_penalty_i.tolist()
-    #         bt.logging.trace(str(penalty_fn_i.name), applied_penalty_i.tolist())
-
-
-    #     # Find the best completion given the rewards vector.
-    #     completions: List[str] = [comp.completion for comp in responses]
-    #     completion_status_message: List[str] = [
-    #         str(comp.dendrite.status_message) for comp in responses
-    #     ]
-    #     completion_status_codes: List[str] = [
-    #         str(comp.dendrite.status_code) for comp in responses
-    #     ]
-
-    #     best: str = ''
-    #     if len(responses) != 0:
-    #         best: str = completions[rewards.argmax(dim=0)].strip()
-
-    #     # Get completion times
-    #     completion_times: List[float] = [
-    #         comp.dendrite.process_time if comp.dendrite.process_time != None else 0
-    #         for comp in responses
-    #     ]
-
-    #     # Compute forward pass rewards, assumes followup_uids and answer_uids are mutually exclusive.
-    #     # shape: [ metagraph.n ]
-    #     scattered_rewards: torch.FloatTensor = self.moving_averaged_scores.scatter(
-    #         0, uids, rewards
-    #     ).to(self.device)
-    #     bt.logging.info(f"Scattered reward: {torch.mean(scattered_rewards)}")
-
-    #     # Update moving_averaged_scores with rewards produced by this step.
-    #     # shape: [ metagraph.n ]
-    #     alpha: float = self.config.neuron.moving_average_alpha
-    #     self.moving_averaged_scores: torch.FloatTensor = alpha * scattered_rewards + (
-    #         1 - alpha
-    #     ) * self.moving_averaged_scores.to(self.device)
-    #     bt.logging.info(f"Moving everaged scores: {torch.mean(self.moving_averaged_scores)}")
-
-    #     # Log the step event.
-    #     event.update(
-    #         {
-    #             # "block": ttl_get_block(self),
-    #             "step_length": time.time() - start_time,
-    #             "prompt": prompt,
-    #             "uids": uids.tolist(),
-    #             "completions": completions,
-    #             "completion_times": completion_times,
-    #             "completion_status_messages": completion_status_message,
-    #             "completion_status_codes": completion_status_codes,
-    #             "rewards": rewards.tolist(),
-    #             "best": best,
-    #         }
-    #     )
-    #     bt.logging.debug("Run Task event:", str(event))
-    #     bt.logging.info(f"Best Response: {event['best']}")
-
-    #     scores = torch.zeros(len(metagraph.hotkeys))
-    #     uid_scores_dict = {}
-    #     for uid, reward, response in zip(uids, rewards.tolist(), responses):
-    #         uid_scores_dict[uid] = reward
-    #         scores[uid] = reward
-    #         self.wandb_data["scores"][uid] = reward
-    #         self.wandb_data["responses"][uid] = response.completion
-    #         self.wandb_data["prompts"][uid] = prompt
-            
-    #     # Return the event.
-    #     return scores, uid_scores_dict, self.wandb_data, event
-    
+        #todo it later
+        # task_name = "augment"
+        # twitter_task = TwitterTask(base_text=prompt, task_name=task_name, task_type="twitter_scraper", criteria=[])
+        # scores, uid_scores_dict, self.wandb_data, event = await self.run_task_and_score(
+        #     task=twitter_task,
+        #     available_uids=available_uids,
+        #     metagraph=metagraph
+        # )
---
+++ neurons/validators/weights.py
@@ -0,0 +1,85 @@
+# The MIT License (MIT)
+# Copyright © 2023 Yuma Rao
+# Copyright © 2023 Opentensor Foundation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+# Utils for weights setting on chain.
+
+import wandb
+import torch
+import bittensor as bt
+import template
+   
+def init_wandb(self):
+    if self.config.wandb_on:
+        run_name = f'validator-{self.my_uuid}-{template.__version__}'
+        self.config.uid = self.my_uuid
+        self.config.hotkey = self.wallet.hotkey.ss58_address
+        self.config.run_name = run_name
+        self.config.version = template.__version__
+        self.config.type = 'validator'
+
+        # Initialize the wandb run for the single project
+        run = wandb.init(
+            name=run_name,
+            project=template.PROJECT_NAME,
+            entity='cortex-t',
+            config=self.config,
+            dir=self.config.full_path,
+            reinit=True
+        )
+
+        # Sign the run to ensure it's from the correct hotkey
+        signature = self.wallet.hotkey.sign(run.id.encode()).hex()
+        self.config.signature = signature 
+        wandb.config.update(self.config, allow_val_change=True)
+
+        bt.logging.success(f"Started wandb run for project '{template.PROJECT_NAME}'")
+
+def set_weights(self, scores):
+    # alpha of .3 means that each new score replaces 30% of the weight of the previous weights
+    alpha = .3
+    if self.moving_average_scores is None:
+        self.moving_average_scores = scores.clone()
+
+    # Update the moving average scores
+    self.moving_average_scores = alpha * scores + (1 - alpha) * self.moving_average_scores
+    bt.logging.info(f"Updated moving average of weights: {self.moving_average_scores}")
+    self.subtensor.set_weights(
+        netuid=self.config.netuid, 
+        wallet=self.wallet, 
+        uids=self.metagraph.uids, 
+        weights=self.moving_average_scores, 
+        wait_for_inclusion=False)
+    bt.logging.success("Successfully set weights.")
+
+def update_weights(self, total_scores, steps_passed):
+    """ Update weights based on total scores, using min-max normalization for display"""
+    avg_scores = total_scores / (steps_passed + 1)
+
+    # Normalize avg_scores to a range of 0 to 1
+    min_score = torch.min(avg_scores)
+    max_score = torch.max(avg_scores)
+    
+    if max_score - min_score != 0:
+        normalized_scores = (avg_scores - min_score) / (max_score - min_score)
+    else:
+        normalized_scores = torch.zeros_like(avg_scores)
+
+    bt.logging.info(f"normalized_scores = {normalized_scores}")
+    # We can't set weights with normalized scores because that disrupts the weighting assigned to each validator class
+    # Weights get normalized anyways in weight_utils
+    set_weights(self, avg_scores)
