b9ec5a70d8bb67b49f550b2ad7befde0eea2a83e
Chkhikvadze <giga.chkhikvadze@gmail.com>
2023-12-27 14:57:29 +0400

    refactor twitter validator class

--- neurons/validators/twitter_validator.py
+++ neurons/validators/twitter_validator.py
@@ -5,7 +5,7 @@ import wandb
 import random
 import json
 import bittensor as bt
-from base_validator import BaseValidator
+from base_validator import AbstractNeuron
 from template.protocol import TwitterScraperStreaming, TwitterPromptAnalysisResult
 from reward import (
     RewardModelType,
@@ -25,25 +25,26 @@ from template.utils import get_random_tweet_prompts
 from template.services.twilio import TwitterAPIClient
 import asyncio
 
-class TwitterScraperValidator(BaseValidator):
-    def __init__(self, dendrite, config, subtensor, wallet, update_score, get_available_uids):
-        super().__init__(dendrite, config, subtensor, wallet, timeout=60)
+class TwitterScraperValidator:
+    def __init__(self, neuron: AbstractNeuron):
         self.streaming = True
         self.query_type = "text"
         self.model = "gpt-4-1106-preview"
         self.weight = 1
         self.seed = 1234
+        self.neuron = neuron
+        self.timeout=75
 
         # Init device.
         bt.logging.debug("loading", "device")
-        self.device = torch.device(self.config.neuron.device)
+        self.device = torch.device(self.neuron.config.neuron.device)
         bt.logging.debug(str(self.device))
 
         self.reward_weights = torch.tensor(
             [
-                self.config.reward.rlhf_weight,
-                self.config.reward.prompt_based_weight,
-                self.config.reward.dpo_weight,
+                self.neuron.config.reward.rlhf_weight,
+                self.neuron.config.reward.prompt_based_weight,
+                self.neuron.config.reward.dpo_weight,
             ],
             dtype=torch.float32,
         ).to(self.device)
@@ -59,15 +60,15 @@ class TwitterScraperValidator(BaseValidator):
     
         self.reward_functions = [
             OpenAssistantRewardModel(device=self.device)
-            if self.config.reward.rlhf_weight > 0
+            if self.neuron.config.reward.rlhf_weight > 0
             else MockRewardModel(RewardModelType.rlhf.value),  
 
             PromptRewardModel(device=self.device)
-            if self.config.reward.prompt_based_weight > 0
+            if self.neuron.config.reward.prompt_based_weight > 0
             else MockRewardModel(RewardModelType.prompt.value),
 
             DirectPreferenceRewardModel(device=self.device)
-            if self.config.reward.dpo_weight > 0
+            if self.neuron.config.reward.dpo_weight > 0
             else MockRewardModel(RewardModelType.prompt.value),                
         ]
 
@@ -85,21 +86,12 @@ class TwitterScraperValidator(BaseValidator):
         }
 
         self.twillio_api = TwitterAPIClient()
-        self.metagraph = None
-        self.neuron = None
-        self.update_score = update_score
-        self.get_available_uids = get_available_uids
-
-    def set_neuron(self, metagraph, update_scores):
-        self.metagraph = metagraph 
-        self.update_scores = update_scores
     
     async def get_uids(self):
-        available_uids = await self.get_available_uids()
+        available_uids = await self.neuron.get_available_uids()
         uid_list = list(available_uids.keys())
         uids = torch.tensor([random.choice(uid_list)]) if uid_list else torch.tensor([])
         return uids
-
 
     async def process_async_responses(self, async_responses):
         responses = []
@@ -129,7 +121,7 @@ class TwitterScraperValidator(BaseValidator):
                 responses.append(synapse_object)
         return responses
 
-    async def run_task_and_score(self, task: TwitterTask, is_scoring_background : False):
+    async def run_task_and_score(self, task: TwitterTask, is_scoring_background: bool = False):
         task_name = task.task_name
         prompt = task.compose_prompt()
 
@@ -140,12 +132,12 @@ class TwitterScraperValidator(BaseValidator):
         start_time = time.time()
         
         # Get random id on that step
-        uids = self.get_uids()
-        axons = [self.metagraph.axons[uid] for uid in uids]
+        uids = await self.get_uids()
+        axons = [self.neuron.metagraph.axons[uid] for uid in uids]
         synapse = TwitterScraperStreaming(messages=prompt, model=self.model, seed=self.seed)
 
         # Make calls to the network with the prompt.
-        async_responses: List[bt.Synapse] = await self.dendrite.forward(
+        async_responses = await self.neuron.dendrite.forward(
             axons=axons,
             synapse=synapse,
             timeout=self.timeout,
@@ -153,20 +145,24 @@ class TwitterScraperValidator(BaseValidator):
             deserialize=False,
         )
 
-        responses = await self.process_async_responses(async_responses)
-    
-        if responses:
-            task.prompt_analysis = responses[0].prompt_analysis
-
         if is_scoring_background:
-            # Run compute_rewards_and_penalties as a background task
-            asyncio.create_task(self.compute_rewards_and_penalties(event, task, responses, uids, start_time))
-            return responses
+            # Process responses asynchronously and compute rewards and penalties in the background
+            async def process_and_score_responses():
+                responses = await self.process_async_responses(async_responses)
+                if responses:
+                    task.prompt_analysis = responses[0].prompt_analysis
+                await self.compute_rewards_and_penalties(event, task, responses, uids, start_time)
+
+            # Start the background task and return the async generator for streaming
+            asyncio.create_task(process_and_score_responses())
+            return self.process_async_responses(async_responses)
         else:
-            # Wait for compute_rewards_and_penalties to complete
+            # In non-streaming context, await all responses and then process
+            responses = await self.process_async_responses(async_responses)
+            if responses:
+                task.prompt_analysis = responses[0].prompt_analysis
             await self.compute_rewards_and_penalties(event, task, responses, uids, start_time)
             return responses
-
     
     async def compute_rewards_and_penalties(self, event, prompt, task, responses, uids, start_time):
         if responses:
@@ -176,14 +172,14 @@ class TwitterScraperValidator(BaseValidator):
         for weight_i, reward_fn_i in zip(self.reward_weights, self.reward_functions):
             reward_i_normalized, reward_event = reward_fn_i.apply(task.base_text, responses, task.task_name)
             rewards += weight_i * reward_i_normalized.to(self.device)
-            if not self.config.neuron.disable_log_rewards:
+            if not self.neuron.config.neuron.disable_log_rewards:
                 event = {**event, **reward_event}
             bt.logging.trace(str(reward_fn_i.name), reward_i_normalized.tolist())
 
         for penalty_fn_i in self.penalty_functions:
             raw_penalty_i, adjusted_penalty_i, applied_penalty_i = penalty_fn_i.apply_penalties(responses, task)
             rewards *= applied_penalty_i.to(self.device)
-            if not self.config.neuron.disable_log_rewards:
+            if not self.neuron.config.neuron.disable_log_rewards:
                 event[penalty_fn_i.name + "_raw"] = raw_penalty_i.tolist()
                 event[penalty_fn_i.name + "_adjusted"] = adjusted_penalty_i.tolist()
                 event[penalty_fn_i.name + "_applied"] = applied_penalty_i.tolist()
@@ -192,7 +188,7 @@ class TwitterScraperValidator(BaseValidator):
         scattered_rewards = self.update_moving_averaged_scores(uids, rewards)
         self.log_event(task, event, start_time, uids, rewards, prompt=task.compose_prompt())
 
-        scores = torch.zeros(len(self.metagraph.hotkeys))
+        scores = torch.zeros(len(self.neuron.metagraph.hotkeys))
         uid_scores_dict = {}
         for uid, reward, response in zip(uids, rewards.tolist(), responses):
             uid_scores_dict[uid] = reward
@@ -201,7 +197,7 @@ class TwitterScraperValidator(BaseValidator):
             self.wandb_data["responses"][uid] = response.completion
             self.wandb_data["prompts"][uid] = prompt
         
-        self.update_scores(scores, self.wandb_data)
+        self.neuron.update_scores(scores, self.wandb_data)
 
         return rewards, scattered_rewards
 
@@ -209,7 +205,7 @@ class TwitterScraperValidator(BaseValidator):
         scattered_rewards = self.moving_averaged_scores.scatter(0, uids, rewards).to(self.device)
         bt.logging.info(f"Scattered reward: {torch.mean(scattered_rewards)}")
 
-        alpha = self.config.neuron.moving_average_alpha
+        alpha = self.neuron.config.neuron.moving_average_alpha
         self.moving_averaged_scores = alpha * scattered_rewards + (1 - alpha) * self.moving_averaged_scores.to(self.device)
         bt.logging.info(f"Moving averaged scores: {torch.mean(self.moving_averaged_scores)}")
 
@@ -221,13 +217,14 @@ class TwitterScraperValidator(BaseValidator):
             "prompt": prompt,
             "uids": uids.tolist(),
             "rewards": rewards.tolist(),
+            "propmt": task.base_text
         })
         bt.logging.debug("Run Task event:", str(event))
     
     async def query_and_score(self):
         # Init Weights.
         bt.logging.debug("loading", "moving_averaged_scores")
-        self.moving_averaged_scores = torch.zeros((self.metagraph.n)).to(self.device)
+        self.moving_averaged_scores = torch.zeros((self.neuron.metagraph.n)).to(self.device)
         bt.logging.debug(str(self.moving_averaged_scores))
 
         prompt = get_random_tweet_prompts(1)[0]
@@ -242,17 +239,16 @@ class TwitterScraperValidator(BaseValidator):
 
     async def organic(self, query):
         prompt = query['content']
-        uid = self.get_uids()
 
         task_name = "augment"
         twitter_task = TwitterTask(base_text=prompt, task_name=task_name, task_type="twitter_scraper", criteria=[])
 
-        responses = await self.run_task_and_score(
+        async_responses = await self.run_task_and_score(
             task=twitter_task,
             is_scoring_background=True
         )
 
-        async for response in self.process_async_responses(uid, responses):
+        async for response in async_responses:
             yield response
 
 
