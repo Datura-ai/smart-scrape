cdc2e3df90346e0659be0c41673a26a454892a26
Giga Chkhikvadze <surcyf@gmail.com>
2023-12-30 03:45:33 +0000

    change subnet

--- neurons/miners/config.py
+++ neurons/miners/config.py
@@ -41,7 +41,7 @@ def get_config() -> "bt.Config":
         help="Chain endpoint to connect to.",
     )
     # Adds override arguments for network and netuid.
-    parser.add_argument("--netuid", type=int, default=1, help="The chain subnet uid.")
+    parser.add_argument("--netuid", type=int, default=22, help="The chain subnet uid.")
 
     parser.add_argument(
         "--miner.root",
