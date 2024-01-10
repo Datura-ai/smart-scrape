cef1616daf0acbb6fbb5f8f89ab406fbbf3fa28b
Chkhikvadze <giga.chkhikvadze@gmail.com>
2024-01-10 13:48:18 +0400

    feat: api tweets event

--- neurons/validators/api.py
+++ neurons/validators/api.py
@@ -7,8 +7,18 @@ import traceback
 from validator import neuron
 import time
 import asyncio
+from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 EXPECTED_ACCESS_KEY = os.environ.get('VALIDATOR_ACCESS_KEY')
 
 neu = neuron()
@@ -22,6 +32,26 @@ async def response_stream(data):
     except Exception as e:
         bt.logging.error(f"error in response_stream {traceback.format_exc()}")
 
+async def response_stream_event(data):
+    try:
+        last_message = data['messages'][-1]
+        merged_chunks = ""
+        async for response in neu.twitter_validator.organic(last_message):
+            # Decode the chunk if necessary and merge
+            chunk = str(response)  # Assuming response is already a string
+            merged_chunks += chunk
+            lines = chunk.split("\n")
+            sse_data = "\n".join(
+                f"data: {line if line else ' '}" for line in lines
+            )
+            print("sse_data: ", sse_data)
+            yield f"{sse_data}\n\n"
+        # Here you might want to do something with merged_chunks
+        # after the loop has finished
+    except Exception as e:
+        bt.logging.error(f"error in response_stream {traceback.format_exc()}")
+        yield f"data: {json.dumps({'error': 'An error occurred'})}\n\n"
+
 
 @app.post("/analyse-tweets")
 async def process_twitter_validatordator(request: Request, data: dict):
@@ -29,13 +59,15 @@ async def process_twitter_validatordator(request: Request, data: dict):
     access_key = request.headers.get("access-key")
     if access_key != EXPECTED_ACCESS_KEY:
         raise HTTPException(status_code=401, detail="Invalid access key")
-    
-    # available_uids = await neu.get_available_uids()
-
-    # if not available_uids:
-    #     raise HTTPException(status_code=503, detail="Miners are not available")
-  
     return StreamingResponse(response_stream(data))
+
+@app.post("/analyse-tweets-event")
+async def process_twitter_validatordator(request: Request, data: dict):
+    # Check access key
+    # access_key = request.headers.get("access-key")
+    # if access_key != EXPECTED_ACCESS_KEY:
+    #     raise HTTPException(status_code=401, detail="Invalid access key")
+    return StreamingResponse(response_stream_event(data))
 
 def run_fastapi():
     uvicorn.run(app, host="0.0.0.0", port=8005)
