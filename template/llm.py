72bdbec59119fc26772f94baa52693ca445685fa
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2024-01-29 21:37:18 +0000

    fix: llm stream

--- template/llm.py
+++ template/llm.py
@@ -23,8 +23,7 @@ class LLMManger:
                  model_provider = ModelProviders.OPEN_AI,
                  model_name = 'gpt-4-1106-preview',
                  temperature = 0.2,
-                 device = 'cpu'
-                 ):
+                 device = 'cpu'):
         if model_provider == ModelProviders.LOCAL.value:
             self.llm = LocalLLM(model_name=model_name,
                                 temperature=temperature,
@@ -33,13 +32,23 @@ class LLMManger:
             self.llm = OpenAILLM(model_name=model_name,
                                  temperature=temperature)
 
-    def prompt(self, messages, temperature, stream=False, response_format=None, model=None, seed=None): 
-        return self.llm.prompt(messages=messages, 
-                               temperature=temperature, 
-                               model=model, 
-                               seed=seed, 
-                               response_format=response_format,
-                               stream=stream)
+    async def prompt(self, messages, temperature, stream=False, response_format=None, model=None, seed=None):
+        if stream:
+            # If streaming is enabled, return the stream directly
+            return await self.llm.prompt_stream(messages=messages, temperature=temperature, model=model, seed=seed, response_format=response_format)
+        else:
+            # If streaming is not enabled, return a single response
+            response = await self.llm.prompt_regular(messages=messages, temperature=temperature, model=model, seed=seed, response_format=response_format)
+            return response  # This is a single response, not an async generator
+
+    async def _prompt_stream(self, messages, temperature, response_format=None, model=None, seed=None):
+        # This method handles the streaming logic
+        async for response in self.llm.prompt_stream(messages=messages, temperature=temperature, model=model, seed=seed, response_format=response_format):
+            yield response
+
+    async def _prompt_regular(self, messages, temperature, response_format=None, model=None, seed=None):
+        # This method handles the non-streaming logic
+        return await self.llm.prompt_regular(messages=messages, temperature=temperature, model=model, seed=seed, response_format=response_format)
 
 class BaseLLM:
     def __init__(self, model_name, temperature, device='cpu'):
@@ -47,17 +56,14 @@ class BaseLLM:
         self.temperature = temperature
         self.device = device
         self.system_prompt = ""
-        self.do_prompt_injection = "Whether to use a custom 'system' prompt instead of the one sent by bittensor."
-        self.do_sample = "Whether to use sampling or not (if not, uses greedy decoding)."
-        self.max_new_tokens = 256
+        self.do_prompt_injection = ""
+        self.do_sample = ""
+        self.max_new_tokens = 4096
         
     @abstractmethod
-    def prompt(self):
+    async def prompt(self):
         pass
 
-    @abstractmethod
-    def prompt_steaming(self):
-        pass
 
 class OpenAILLM(BaseLLM):
     def __init__(self, model_name, temperature):
@@ -107,19 +113,21 @@ class LocalLLM(BaseLLM):
         bt.logging.info("Loading " + str(self.model_name))
         self.tokenizer = AutoTokenizer.from_pretrained(
             self.model_name, 
-            use_fast=False
+            use_fast=False,
+            token="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"
         )
         self.model = AutoModelForCausalLM.from_pretrained(
             self.model_name,
             torch_dtype=torch.float16,
             low_cpu_mem_usage=True,
+            token="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"
         )
         bt.logging.info("Model loaded!")
 
         if self.device != "cpu":
             self.model = self.model.to(self.device)
     
-    def _process_history(self, roles: List[str], messages: List[str]) -> str:
+    def _process_history(self, messages) -> str:
         """
         Processes message history by concatenating roles and messages.
 
@@ -133,34 +141,59 @@ class LocalLLM(BaseLLM):
         processed_history = ""
         if self.do_prompt_injection:
             processed_history += self.system_prompt
-        for role, message in zip(roles, messages):
+        for item in messages:
+            role = item['role']
+            message = item['content']
             if role == "system":
                 if not self.do_prompt_injection or message != message[0]:
                     processed_history += "" + message.strip() + " "
-            if role == "Assistant":
+            if role == "assistant":
                 processed_history += "ASSISTANT:" + message.strip() + "</s>"
             if role == "user":
                 processed_history += "USER: " + message.strip() + " "
         return processed_history
 
-    def prompt(self, messages: List[any], stream: True) -> str:
-        # history = self._process_history(roles=synapse.roles, messages=synapse.messages)
-        # prompt = history + "ASSISTANT:"
-        prompt = ''
+
+    async def prompt_stream(self, messages, temperature, response_format=None, model=None, seed=None):
+        # This function is for streaming (async generator)
+        history = self._process_history(messages=messages)
+        prompt = history + "ASSISTANT:"
         input_ids = self.tokenizer.encode(prompt, return_tensors="pt").to(self.device)
-        output = self.model.generate(
-            input_ids,
-            max_length=input_ids.shape[1] + self.max_new_tokens,
-            temperature=self.temperature,
-            do_sample=self.do_sample,
-            pad_token_id=self.tokenizer.eos_token_id,
-        )
 
-        completion = self.tokenizer.decode(
-            output[0][input_ids.shape[1] :], skip_special_tokens=True
-        )
+        # Define a synchronous wrapper function for the generate method
+        def generate_sync():
+            return self.model.generate(
+                input_ids,
+                max_length=input_ids.shape[1] + self.max_new_tokens,
+                temperature=temperature or self.temperature,
+                do_sample=self.do_sample,
+                pad_token_id=self.tokenizer.eos_token_id,
+            )
 
-        # Logging input and generation if debugging is active
-        bt.logging.debug("Message: " + str(messages))
-        bt.logging.debug("Generation: " + str(completion))
-        return completion
+        # Run the synchronous generate function in an executor
+        output = await asyncio.get_event_loop().run_in_executor(None, generate_sync)
+
+        # Yield each token as it's generated
+        for token_id in output[0][input_ids.shape[1]:]:
+            yield self.tokenizer.decode(token_id, skip_special_tokens=True)
+
+    async def prompt_regular(self, messages, temperature, response_format=None, model=None, seed=None):
+        try:
+            # This function is for regular async function logic
+            history = self._process_history(messages=messages)
+            prompt = history + "ASSISTANT:"
+            input_ids = self.tokenizer.encode(prompt, return_tensors="pt").to(self.device)
+
+            output = self.model.generate(
+                input_ids,
+                max_length=input_ids.shape[1] + self.max_new_tokens,
+                temperature=temperature or self.temperature,
+                do_sample=self.do_sample,
+                pad_token_id=self.tokenizer.eos_token_id,
+            )
+            completion = self.tokenizer.decode(output[0][input_ids.shape[1]:], skip_special_tokens=True)
+            bt.logging.debug("Generation: " + str(completion))
+            return completion
+        except Exception as e:
+            bt.logging.error(f"analyse_prompt_and_fetch_tweets, {e}")
+            raise e
