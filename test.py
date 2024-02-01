ec0e79275d261eaa98bd2d5d035e6cd89177d446
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2024-02-02 00:25:39 +0400

    fix: test.py error close session

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ message = "tell me about sports"
 
 axon = bt.axon(wallet=wallet)
 dendrite = bt.dendrite(wallet=wallet)
+bt.debug(True)
 
 # construct the synapse from the protocol using the message and whatever else we need
 synapse = TwitterScraperStreaming(messages=message, model="gpt-4-1106-preview", seed=1234)
@@ -23,28 +24,31 @@ meta = bt.metagraph(netuid = 22)
 axons = [meta.axons[uid] for uid in uids]
 
 async def main():
-    async_responses = await dendrite.forward(
-        axons=axons, 
-        synapse=synapse, 
-        deserialize=False,
-        timeout=120,
-        streaming=True,
-    )
-    full_response = ""
-    for resp in async_responses:
-        async for chunk in resp:
-            if isinstance(chunk, str):
-                bt.logging.trace(chunk)
-                full_response += chunk
-        bt.logging.debug(f"full_response: {full_response}")
-        break
-    # responses = await process_async_responses(async_responses, message)
-    # for response in responses:
-    #     print(response)
+    bt.logging.debug(f"Connecting miner uids {uids}")
+    async with bt.dendrite(wallet=wallet) as dendrite:
+        async_responses = await dendrite.forward(
+            axons=axons, 
+            synapse=synapse, 
+            deserialize=False,
+            timeout=120,
+            streaming=True,
+        )
+        full_response = ""
+        response_received = False
+        for resp in async_responses:
+            async for chunk in resp:
+                if isinstance(chunk, str):
+                    bt.logging.trace(chunk)
+                    full_response += chunk
+                    response_received = True
+            if not response_received:
+                bt.logging.debug("No response from miner")
+                break
+            bt.logging.debug(f"Full_response: {full_response}")
+    bt.logging.debug("Finished connecting miner")
 
 if __name__ == "__main__":
     asyncio.run(main())
-
 
 
     
