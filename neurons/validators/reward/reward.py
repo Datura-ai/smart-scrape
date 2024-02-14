fafa295cefd7ab6785cd43a45c02bd15b9adb9a6
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2024-02-14 19:01:45 +0400

    refactor: reward py

--- neurons/validators/reward/reward.py
+++ neurons/validators/reward/reward.py
@@ -46,8 +46,7 @@ class BaseRewardEvent:
 class BaseRewardModel:
     @property
     @abstractmethod
-    def name(self) -> str:
-        ...
+    def name(self) -> str: ...
 
     def __str__(self) -> str:
         return str(self.name)
@@ -58,70 +57,24 @@ class BaseRewardModel:
     @abstractmethod
     def get_rewards(
         self, prompt: str, responses: List[bt.Synapse], name: str
-    ) -> Union[torch.FloatTensor, dict]:
-        ...
+    ) -> Union[torch.FloatTensor, dict]: ...
 
     def __init__(self) -> None:
         self.count = 0
         self.mean = 0.0
         self.var = 0.0
-        self.count_limit = 3000
 
     def normalize_rewards(self, rewards: torch.FloatTensor) -> torch.FloatTensor:
-        """
-        This method normalizes the given rewards by updating the moving mean and variance statistics. The rewards are first standardized, and then scaled to the 0-1 range using a cumulative distribution function (CDF) to ensure they're in a comparable range across different environments.
-
-        Args:
-        rewards (torch.FloatTensor): The reward values to be normalized.
-
-        Returns:
-        torch.FloatTensor: The normalized reward values.
-
-        Note:
-        - This function uses Welford's online algorithm to update the mean and variance.
-        - It standardizes the reward values using the updated mean and variance.
-        - It then scales the standardized values to the 0-1 range using the error function (erf) as a CDF.
-        """
-        # Get the number of rewards (successful responses).
-        new_count = rewards.numel()
-
-        # # Update stats only if there are new rewards.
-        # if 0 < new_count and 0 < self.count + new_count:
-        #     # Calculate the mean and standard deviation of the new rewards.
-        #     new_mean = rewards.mean()
-        #     new_var = rewards.var(dim=0)
-
-        #     # Compute the weights for the new and old rewards.
-        #     new_weight = new_count / (self.count + new_count)
-        #     old_weight = self.count / (self.count + new_count)
-
-        #     # Save the difference in means before updating the old mean.
-        #     diff = new_mean - self.mean
-
-        #     # Update the old mean with the new mean and weights.
-        #     self.mean = new_weight * new_mean + old_weight * self.mean
-        #     # Update the old variance with the new variance and weights, and adjusting for the difference in means.
-        #     self.var = (
-        #         (new_weight * new_var)
-        #         + (old_weight * self.var)
-        #         + (new_weight * old_weight) * diff * diff
-        #     )
-        #     # Update the old count with the new count, but don't exceed the limit.
-        #     self.count = min(self.count_limit, self.count + new_count)
-
-        # Standardize the rewards using the updated mean and variance.
-        # rewards = rewards - self.mean
         if self.var > 0:
             rewards /= torch.sqrt(self.var)
-        # Scale the standardized rewards to the range [0, 1] using the error function as a cumulative distribution function (CDF).
-        # rewards = 0.5 * (
-        #     1 + torch.erf(rewards / torch.sqrt(torch.tensor([2.0])).to(rewards.device))
-        # )
-        common_formula = torch.erf(rewards / torch.sqrt(torch.tensor([2.0])).to(rewards.device))
+
+        common_formula = torch.erf(
+            rewards / torch.sqrt(torch.tensor([2.0])).to(rewards.device)
+        )
         rewards = torch.where(rewards == 0, 0, 0.5 * (1 + common_formula))
 
         return rewards
-    
+
     def get_successful_completions(self, responses: List[bt.Synapse]):
         successful_completions_indices: List[int] = [
             idx
@@ -135,7 +88,7 @@ class BaseRewardModel:
         ]
 
         return successful_completions
-    
+
     def get_successful_completion(self, response: bt.Synapse):
         # Check if the response is successful.
         if response.dendrite.status_code == 200 and response.links_content:
@@ -172,14 +125,6 @@ class BaseRewardModel:
         filled_rewards = torch.ones(len(responses), dtype=torch.float32) * torch.nan
         filled_rewards_normalized = torch.zeros(len(responses), dtype=torch.float32)
 
-        # Fill reward tensor.
-        # for idx, reward, reward_normalized in zip(
-        #     successful_completions_indices,
-        #     successful_rewards,
-        #     successful_rewards_normalized,
-        # ):
-        #     filled_rewards[idx] = reward
-        #     filled_rewards_normalized[idx] = reward_normalized
         for idx in successful_completions_indices:
             filled_rewards[idx] = successful_rewards[idx]
             filled_rewards_normalized[idx] = successful_rewards_normalized[idx]
