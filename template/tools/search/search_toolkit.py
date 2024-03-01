399fb4a2fd6bafb0a819101206496e893ccf4e10
Mirian Okradze <okradzemirian@gmail.com>
2024-03-01 20:53:41 +0400

    feat: add youtube search tool in search toolkit

--- template/tools/search/search_toolkit.py
+++ template/tools/search/search_toolkit.py
@@ -3,6 +3,7 @@ from typing import List
 from template.tools.base import BaseToolkit, BaseTool
 from .serp_google_search_tool import SerpGoogleSearchTool
 from .wikipedia_search_tool import WikipediaSearchTool
+from .youtube_search_tool import YoutubeSearchTool
 from .search_summary import summarize_search_data
 
 
@@ -16,7 +17,7 @@ class SearchToolkit(BaseToolkit, ABC):
     toolkit_id = "fed46dde-ee8e-420b-a1bb-4a161aa01dca"
 
     def get_tools(self) -> List[BaseTool]:
-        return [SerpGoogleSearchTool(), WikipediaSearchTool()]
+        return [SerpGoogleSearchTool(), WikipediaSearchTool(), YoutubeSearchTool()]
 
     async def summarize(self, prompt, model, data):
         return await summarize_search_data(prompt=prompt, model=model, data=data)
