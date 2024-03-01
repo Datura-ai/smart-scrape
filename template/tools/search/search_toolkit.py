481a1e5199a82e422cba88e93c8e6b5e90ccb829
Mirian Okradze <okradzemirian@gmail.com>
2024-03-01 21:49:38 +0400

    feat: arxiv search tool

--- template/tools/search/search_toolkit.py
+++ template/tools/search/search_toolkit.py
@@ -4,6 +4,7 @@ from template.tools.base import BaseToolkit, BaseTool
 from .serp_google_search_tool import SerpGoogleSearchTool
 from .wikipedia_search_tool import WikipediaSearchTool
 from .youtube_search_tool import YoutubeSearchTool
+from .arxiv_search_tool import ArxivSearchTool
 from .search_summary import summarize_search_data
 
 
@@ -17,7 +18,12 @@ class SearchToolkit(BaseToolkit, ABC):
     toolkit_id = "fed46dde-ee8e-420b-a1bb-4a161aa01dca"
 
     def get_tools(self) -> List[BaseTool]:
-        return [SerpGoogleSearchTool(), WikipediaSearchTool(), YoutubeSearchTool()]
+        return [
+            SerpGoogleSearchTool(),
+            WikipediaSearchTool(),
+            YoutubeSearchTool(),
+            ArxivSearchTool(),
+        ]
 
     async def summarize(self, prompt, model, data):
         return await summarize_search_data(prompt=prompt, model=model, data=data)
