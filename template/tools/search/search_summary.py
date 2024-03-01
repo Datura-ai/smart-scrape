793552728f9c673cf61dd8431df9491a4fe39616
Mirian Okradze <okradzemirian@gmail.com>
2024-03-01 20:29:36 +0400

    feat: add wikipedia search tool and integrate in search toolkit

--- template/tools/search/search_summary.py
+++ template/tools/search/search_summary.py
@@ -5,22 +5,22 @@ client = AsyncOpenAI(timeout=60.0)
 
 
 SYSTEM_MESSAGE = """
-As Google search data analyst, your task is to provide users with a clear and concise summary derived from the given Google search data and the user's query.
+As search data analyst, your task is to provide users with a clear and concise summary derived from the given search data and the user's query.
 
 Tasks:
-1. Relevant Links: Provide a selection of Google search links that directly correspond to the <UserPrompt>. For each link, include a concise explanation that connects its relevance to the user's question.
-Synthesize insights from both the <UserPrompt> and the <GoogleSearch> to formulate a well-rounded response.
+1. Relevant Links: Provide a selection of search links that directly correspond to the <UserPrompt>. For each link, include a concise explanation that connects its relevance to the user's question.
+Synthesize insights from both the <UserPrompt> and the <SearchData> to formulate a well-rounded response.
 2. Highlight Key Information: Identify and emphasize any crucial information that will be beneficial to the user.
 
 Output Guidelines (Tasks):
-1. Relevant Links: Provide a selection of Google links that directly correspond to the <UserPrompt>. For each link, include a concise explanation that connects its relevance to the user's question.
-Synthesize insights from both the <UserPrompt> and the <GoogleSearch> to formulate a well-rounded response.
+1. Relevant Links: Provide a selection of links that directly correspond to the <UserPrompt>. For each link, include a concise explanation that connects its relevance to the user's question.
+Synthesize insights from both the <UserPrompt> and the <SearchData> to formulate a well-rounded response.
 2. Highlight Key Information: Identify and emphasize any crucial information that will be beneficial to the user.
 
 Operational Rules:
-1. No <GoogleSearch> Scenario: If no GoogleSearch is provided, inform the user that current Google insights related to their topic are unavailable.
+1. No <SearchData> Scenario: If no SearchData is provided, inform the user that current insights related to their topic are unavailable.
 2. Emphasis on Critical Issues: Focus on and clearly explain any significant issues or points of interest that emerge from the analysis.
-3. Seamless Integration: Avoid explicitly stating "Based on the provided <GoogleSearch>" in responses. Assume user awareness of the data integration process.
+3. Seamless Integration: Avoid explicitly stating "Based on the provided <SearchData>" in responses. Assume user awareness of the data integration process.
 4. Please separate your responses into sections for easy reading.
 5. Not return text like <UserPrompt> to your response, make response easy to understand to any user.
 6. Make headers bold using Markdown.
@@ -31,15 +31,15 @@ Operational Rules:
 async def summarize_search_data(prompt: str, model: str, data):
     content = f"""
     In <UserPrompt> provided User's prompt (Question).
-    In <GoogleSearch> I fetch data from Google search API.
+    In <SearchData> I fetch data from Google, Youtube or Wikipedia.
 
     <UserPrompt>
     {prompt}
     </UserPrompt>
 
-    <GoogleSearch>
+    <SearchData>
     {data}
-    </GoogleSearch>
+    </SearchData>
     """
 
     messages = [
--- template/tools/search/search_toolkit.py
+++ template/tools/search/search_toolkit.py
@@ -2,6 +2,7 @@ from abc import ABC
 from typing import List
 from template.tools.base import BaseToolkit, BaseTool
 from .serp_google_search_tool import SerpGoogleSearchTool
+from .wikipedia_search_tool import WikipediaSearchTool
 from .search_summary import summarize_search_data
 
 
@@ -15,7 +16,7 @@ class SearchToolkit(BaseToolkit, ABC):
     toolkit_id = "fed46dde-ee8e-420b-a1bb-4a161aa01dca"
 
     def get_tools(self) -> List[BaseTool]:
-        return [SerpGoogleSearchTool()]
+        return [SerpGoogleSearchTool(), WikipediaSearchTool()]
 
     async def summarize(self, prompt, model, data):
         return await summarize_search_data(prompt=prompt, model=model, data=data)
