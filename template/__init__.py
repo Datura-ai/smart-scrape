874a96062d1884d868228536a17e50b67401a2fd
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2024-03-06 16:39:04 +0400

    fix: version 0.0.52

--- template/__init__.py
+++ template/__init__.py
@@ -19,7 +19,7 @@
 
 
 # version must stay on line 22
-__version__ = "0.0.51"
+__version__ = "0.0.52"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
