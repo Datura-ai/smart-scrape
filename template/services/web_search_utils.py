9a5512453c2e7f0d71fdc4c6d971b0fdebe40314
Mirian Okradze <okradzemirian@gmail.com>
2024-03-24 15:17:28 +0400

    fix: find links regex in web search utils

--- template/services/web_search_utils.py
+++ template/services/web_search_utils.py
@@ -1,15 +1,14 @@
 import re
+
 from typing import List
 
 
 class WebSearchUtils:
     @staticmethod
     def find_links(text: str) -> List[str]:
-        link_regex = r"(?:^|\n)\s*(?:\d+\s*)?([^\n]*?)\s*\[([^\]]*?)\]\((.*?)\)"
+        link_regex = r"\[.*?\]\((https?://[^\s\)]+)\)"
         links = []
-
-        for match in re.finditer(link_regex, text, re.MULTILINE):
-            link = match.group(3)
+        for match in re.finditer(link_regex, text):
+            link = match.group(1)
             links.append(link)
-
         return links
