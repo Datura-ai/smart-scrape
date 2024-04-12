258952f6365496034e4204530ade79d037c42f13
sirouk <8901571+sirouk@users.noreply.github.com>
2024-04-12 00:46:36 -0400

    Update config.py
    Added external_ip to the argument parser and the default is using a native bittensor function call for external IP (str):
    
    https://github.com/opentensor/bittensor/blob/bfc78773788cb8d349c51fe4e4dd9c88ee520a1d/bittensor/utils/networking.py#L90

--- neurons/miners/config.py
+++ neurons/miners/config.py
@@ -28,6 +28,10 @@ def get_config() -> "bt.Config":
     parser.add_argument(
         "--axon.port", type=int, default=8098, help="Port to run the axon on."
     )
+    # External IP 
+    parser.add_argument(
+        "--axon.external_ip", type=str, default=bt.utils.networking.get_external_ip(), help="IP for the metagraph"
+    )
     # Subtensor network to connect to
     parser.add_argument(
         "--subtensor.network",
