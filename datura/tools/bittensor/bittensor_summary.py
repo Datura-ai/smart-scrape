99d0e016953e48f15f2f05990df8071098ca5a2c
Ismael Sh <me@theiskaa.com>
2024-05-01 23:14:33 +0400

    feat(bittensor-docs): improve summary

--- datura/tools/bittensor/bittensor_summary.py
+++ datura/tools/bittensor/bittensor_summary.py
@@ -5,10 +5,10 @@ client = AsyncOpenAI(timeout=60.0)
 
 
 SYSTEM_MESSAGE = """
-As a Bittensor Documentation data analyst, your task is to provide users with a clear and concise answer derived from the given Bittensor Documentation conversation and the user's query.
+As a Bittensor Documentation data analyst, your task is to provide users with a clear and concise answer derived from the given Bittensor Documentation and the user's query.
 
 Output Guidelines (Tasks):
-1. Analyze the user's prompt and the provided Bittensor Documentation data and write a well-rounded answer that addresses the user's query.
+1. Analyze the user's prompt and the provided Bittensor Documentation data and write a well-rounded and detailed answer that addresses the user's query.
 
 <OutputExample>
 **Bittensor Documentation Summary:**
@@ -46,8 +46,8 @@ Operational Rules:
 2. Emphasis on Critical Issues: Focus on and clearly explain any significant issues or points of interest that emerge from the analysis.
 3. Seamless Integration: Avoid explicitly stating "Based on the provided Bittensor Documentation data" in responses. Assume user awareness of the data integration process.
 5. User-Friendly Language: Do not return text like <UserPrompt>; make responses easy to understand for any user.
-6. Use Markdown: Make headers bold using Markdown, code blocks with markdown code blocks.
-7. Provide Links with appropriate titles: Include links to relevant resources or information. And, always include the current document's original URL link for reference.
+6. Use Markdown: Make headers bold using Markdown, code blocks with markdown code blocks, and lists with markdown lists.
+7. Provide Links with Unique and Descriptive Titles: Include links to relevant resources or information, and ensure that the link titles (the text that appears within the square brackets) are unique and descriptive, providing relevant information about the content or purpose of the linked resource. The link titles should be generated based on the URL itself, rather than using generic or repetitive text. For instance, if the URL is a file path within a specific repository, the link title can include the file name along with the repository name. Always include the current document's original URL link for reference.
 """
 
 
