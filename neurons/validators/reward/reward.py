fix: normalization code

neurons/validators/reward/reward.py
@@ -83,23 +83,25 @@ class BaseRewardModel:
         max_reward = (
             torch.max(rewards[~zero_mask]) if rewards[~zero_mask].nelement() > 0 else 1
         )
-        epsilon = 1e-10
-        rewards[~zero_mask] = (rewards[~zero_mask] - min_reward) / (
-            max(max_reward - min_reward, epsilon)
-        )
 
-        # Apply a more aggressive exponential function to exaggerate differences
-        # Adjust the exponent to control the differentiation between scores
-        exaggeration_factor = (
-            3  # Adjust this factor to control the steepness of the reward curve
-        )
-        rewards[~zero_mask] = torch.pow(
-            rewards[~zero_mask] * exaggeration_factor, exaggeration_factor
-        )
+        # Check if all non-zero rewards are the same
+        if min_reward == max_reward:
+            rewards[~zero_mask] = 1  # Set all non-zero rewards to 1
+        else:
+            epsilon = 1e-10
+            rewards[~zero_mask] = (rewards[~zero_mask] - min_reward) / (
+                max(max_reward - min_reward, epsilon)
+            )
 
-        # Re-scale to ensure the top score is close to 1 after exponential exaggeration
-        if rewards[~zero_mask].nelement() > 0:
-            rewards[~zero_mask] /= torch.max(rewards[~zero_mask])
+            # Apply a more aggressive exponential function to exaggerate differences
+            exaggeration_factor = 3
+            rewards[~zero_mask] = torch.pow(
+                rewards[~zero_mask] * exaggeration_factor, exaggeration_factor
+            )
+
+            # Re-scale to ensure the top score is close to 1 after exponential exaggeration
+            if rewards[~zero_mask].nelement() > 0:
+                rewards[~zero_mask] /= torch.max(rewards[~zero_mask])
 
         # Ensure rewards that were initially 0 remain 0
         rewards[zero_mask] = 0