02501cfce430d4717244f1fcb217d19a8a081574
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2024-05-19 17:18:38 +0400

    fix: whitelist

--- datura/__init__.py
+++ datura/__init__.py
@@ -19,7 +19,7 @@
 
 
 # version must stay on line 22
-__version__ = "0.0.101"
+__version__ = "0.0.102"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
@@ -76,7 +76,6 @@ valid_validators = [
     "5GVpVH7DjYmQY7ckznVnrHncU9knzYJvhY3TfbFY7sPboJB2",
     "5Fq5v71D4LX8Db1xsmRSy6udQThcZ8sFDqxQFwnUZ1BuqY5A",
     "5ChuGqW2cxc5AZJ29z6vyTkTncg75L9ovfp8QN8eB8niSD75",
-    "5CkHmNfYSaz1Tpo599WBRsr8tRbMe8c2fT14dNZVYC5Dw7yc",
 ]
 
 WHITELISTED_KEYS = testnet_key + test_key + valid_validators
