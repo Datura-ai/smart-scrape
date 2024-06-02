b8bf27f885fc91c1e0c55abcbe10530bacbe959f
Ismael Sh <me@theiskaa.com>
2024-06-02 23:31:39 +0400

    feat(scripts): improve token generator to automatically embbed as jwt

--- datura/scripts/token_generator.py
+++ datura/scripts/token_generator.py
@@ -1,23 +1,37 @@
 import sys
+import jwt
 import bittensor as bt
+import datetime
 
 
-def generate_token(value: str, coldkey: str, hotkey: str):
+def generate_token(secret: str, coldkey: str, hotkey: str):
     try:
+        jwt_secret = create_jwt(secret)
         wallet = bt.wallet(name=coldkey, hotkey=hotkey)
-        signature = wallet.hotkey.sign(value.encode()).hex()
-        print(f"Signature:\n  {signature}")
+        signature = wallet.hotkey.sign(secret.encode()).hex()
+        print(f"\nSecret:\n  {secret}")
+        print(f"\nAccess Token:\n  {jwt_secret}")
+        print(f"\nSignature:\n  {signature}\n")
     except Exception as e:
         print(f"[!] Error: {e}")
 
 
+def create_jwt(secret: str):
+    payload = {
+        "data": secret,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+    }
+    token = jwt.encode(payload, secret, algorithm="HS256")
+    return token
+
+
 if __name__ == "__main__":
     if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv) < 4:
-        args = '"random cahrachters from our frontend" [coldkey] [hotkey]'
+        args = '[jwt_secret] [coldkey] [hotkey]'
         print(f'\nUsage:\n python token_generator.py {args}\n')
         sys.exit(1)
 
-    value = sys.argv[1]
+    secret = sys.argv[1]
     coldkey = sys.argv[2]
     hotkey = sys.argv[3]
-    generate_token(value, coldkey, hotkey)
+    generate_token(secret, coldkey, hotkey)
