40e7cd466b6d15f0cc83b97efa60f07602229d0c
Ismael Sh <me@theiskaa.com>
2024-08-26 17:30:14 +0400

    fix(scraper_validator): param passing issue

--- neurons/validators/scraper_validator.py
+++ neurons/validators/scraper_validator.py
@@ -172,7 +172,7 @@ class ScraperValidator:
         language="en",
         region="us",
         google_date_filter="qdr:w",
-        response_order=ResponseOrder.SUMMARY_FIRST,
+        response_order=ResponseOrder.SUMMARY_FIRST.value,
     ):
         try:
             task_name = task.task_name
@@ -206,7 +206,7 @@ class ScraperValidator:
                 language=language,
                 region=region,
                 google_date_filter=google_date_filter,
-                response_order=response_order.value,
+                response_order=response_order,
             )
 
             # Make calls to the network with the prompt.
