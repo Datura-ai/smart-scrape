5e9c7cb78973a5fab201df537e3ca1907ee4f912
Mirian Okradze <okradzemirian@gmail.com>
2024-10-23 15:33:36 +0400

    feat: choose random links from each search summary

--- datura/protocol.py
+++ datura/protocol.py
@@ -349,12 +349,16 @@ class ScraperStreamingSynapse(bt.StreamingSynapse):
         In Reddit and Hacker News Search, the links are filtered by domains.
         In search summary part, if Google Search or Google News Search is used, the links are allowed from any domain,
         Otherwise search summary will only look for Wikipedia, ArXiv, Youtube links.
+        Returns list of all links and links per each summary role.
         """
 
         completions, _ = self.get_search_completion()
-        links = []
+        all_links = []
+        links_per_summary = {}
 
         for key, value in completions.items():
+            links = []
+
             if key == ScraperTextRole.REDDIT_SUMMARY.value:
                 links.extend(WebSearchUtils.find_links_by_domain(value, "reddit.com"))
             elif key == ScraperTextRole.HACKER_NEWS_SUMMARY.value:
@@ -381,7 +385,10 @@ class ScraperStreamingSynapse(bt.StreamingSynapse):
                             WebSearchUtils.find_links_by_domain(value, "youtube.com")
                         )
 
-        return links
+            all_links.extend(links)
+            links_per_summary[key] = links
+
+        return all_links, links_per_summary
 
     async def process_streaming_response(self, response: StreamingResponse):
         if self.completion is None:
@@ -541,7 +548,7 @@ class ScraperStreamingSynapse(bt.StreamingSynapse):
             }
 
         completion_links = TwitterUtils().find_twitter_links(self.completion)
-        search_completion_links = self.get_search_links()
+        search_completion_links, _ = self.get_search_links()
 
         return {
             "name": headers.get("name", ""),
