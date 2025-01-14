d48b031a6d2a9abddd1350deb9bf7f55c026075e
Mirian Okradze <okradzemirian@gmail.com>
2025-01-14 12:37:34 +0400

    fix: miner axon serve

--- neurons/miners/miner.py
+++ neurons/miners/miner.py
@@ -322,7 +322,7 @@ class StreamMiner(ABC):
         bt.logging.info(
             f"Serving axon {ScraperStreamingSynapse} on network: {self.config.subtensor.chain_endpoint} with netuid: {self.config.netuid}"
         )
-        self.axon.serve(netuid=self.config.netuid, subtensor=self.subtensor)
+        self.subtensor.serve_axon(axon=self.axon, netuid=self.config.netuid)
         bt.logging.info(f"Starting axon server on port: {self.config.axon.port}")
         self.axon.start()
         self.last_epoch_block = self.subtensor.get_current_block()
